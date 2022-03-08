d26bff4f287ae4c9d716b6dd96a59d9243de03a9
unknown <talk2archan@gmail.com>
2022-03-08 16:44:27 -0500

    more refactoring

--- src/runner.py
+++ src/runner.py
@@ -12,39 +12,39 @@ def train(model, train_loader, valloader, optimizer, num_epochs, criterion):
     tr_acc_all_epochs = []
     val_acc_epochs = []
     for epoch_ in tqdm(range(num_epochs)):
-      total_loss = 0
-      total_acc = 0
+        total_loss = 0
+        total_acc = 0
 
-      counter = 0
-      for i in train_loader:
-          counter += 1
-          
-          data, target = i
-        
-          #FORWARD PASS
-          output = model(data.float())
-          loss = criterion(output, target.unsqueeze(1)) 
-          total_loss += loss.detach()
-          
-          #BACKWARD AND OPTIMIZE
-          optimizer.zero_grad()
-          loss.backward()
+        counter = 0
+        for i in train_loader:
+            counter += 1
 
-          optimizer.step()
-          
-          # PREDICTIONS 
-          pred = np.round(output.detach())
-          target = np.round(target.detach())             
-          y_pred.extend(pred.tolist())
-          y_true.extend(target.tolist())
+            data, target = i
 
-          total_acc += accuracy_score(y_true,y_pred)
+            #FORWARD PASS
+            output = model(data.float())
+            loss = criterion(output, target.unsqueeze(1)) 
+            total_loss += loss.detach()
+
+            #BACKWARD AND OPTIMIZE
+            optimizer.zero_grad()
+            loss.backward()
+
+            optimizer.step()
+
+            # PREDICTIONS 
+            pred = np.round(output.detach())
+            target = np.round(target.detach())             
+            y_pred.extend(pred.tolist())
+            y_true.extend(target.tolist())
+
+            total_acc += accuracy_score(y_true,y_pred)
 
 
-      val_acc_epochs.append(test(model, valloader))
-      loss_all_epochs.append(total_loss/counter)
-      tr_acc_all_epochs.append(total_acc/counter)
-      return model, loss_all_epochs, tr_acc_all_epochs, val_acc_epochs
+        val_acc_epochs.append(test(model, valloader))
+        loss_all_epochs.append(total_loss/counter)
+        tr_acc_all_epochs.append(total_acc/counter)
+    return model, loss_all_epochs, tr_acc_all_epochs, val_acc_epochs
 
 
 #TESTING THE MODEL
